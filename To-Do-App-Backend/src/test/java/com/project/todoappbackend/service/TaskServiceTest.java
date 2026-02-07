b62efddc38702c402705d664fecbe6d58f1724c3
ManishDhamala <dhamalamanish32@gmail.com>
2026-02-07 23:06:30 +0545

    test: Task update unit testing

--- To-Do-App-Backend/src/test/java/com/project/todoappbackend/service/TaskServiceTest.java
+++ To-Do-App-Backend/src/test/java/com/project/todoappbackend/service/TaskServiceTest.java
@@ -10,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -62,6 +64,7 @@ class TaskServiceTest {
         assertThat(result.getTask()).isEqualTo("Test task");
         assertThat(result.getCompleted()).isFalse();
 
+         // Verify mocks were called properly
         verify(taskMapper, times(1)).toEntity(inputDto);
         verify(taskRepository, times(1)).save(taskEntity);
         verify(taskMapper, times(1)).toDTO(savedTaskEntity);
@@ -70,7 +73,59 @@ class TaskServiceTest {
     }
 
     @Test
-    void updateTask() {
+    void updateTask_AndReturnDto() {
+
+        Long taskId = 1L;
+
+        // Arrange
+
+        // Input Dto for update
+        TaskDto inputDto = new TaskDto();
+        inputDto.setId(taskId);
+        inputDto.setTask("Update Task");
+        inputDto.setCompleted(true);
+
+        // Old task
+        Task existingTask = new Task();
+        existingTask.setId(taskId);
+        existingTask.setTask("Old Task");
+        existingTask.setCompleted(false);
+
+
+        // Check after update
+        Task updatedTask = new Task();
+        updatedTask.setId(1L);
+        updatedTask.setTask("Update Task");
+        updatedTask.setCompleted(false);
+
+        // Expected return dto
+        TaskDto expectedDto = new TaskDto();
+        expectedDto.setId(1L);
+        expectedDto.setTask("Update Task");
+        expectedDto.setCompleted(true);
+
+        when(taskRepository.findById(taskId)).thenReturn(Optional.of(existingTask));
+
+        when(taskRepository.save(existingTask)).thenReturn(updatedTask);
+
+        when(taskMapper.toDTO(updatedTask)).thenReturn(expectedDto);
+
+
+        // Act
+        TaskDto result = taskService.updateTask(inputDto);
+
+        //Assert
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getTask()).isEqualTo("Update Task");
+        assertThat(result.getCompleted()).isTrue();
+
+
+        // Verify mocks were called properly
+        verify(taskRepository, times(1)).findById(taskId);
+        verify(taskRepository, times(1)).save(existingTask);
+        verify(taskMapper, times(1)).toDTO(updatedTask);
+
     }
 
     @Test
